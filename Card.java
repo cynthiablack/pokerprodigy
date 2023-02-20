34116e74c8730eaf64d6f5c2e99e18275f58a50e
Cynthia Black <cindycblack@gmail.com>
2023-02-19 16:02:38 -0900

    Add Card class, refactor Rank to include labels

---
+++ Card.java
@@ -0,0 +1,26 @@
+public class Card {
+  // use Suit, Rank enums
+  private Suit suit;
+  private Rank rank;
+
+  // create a new card
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  public String getRank() {
+    return rank.printRankLabel();
+  }
+
+  public String getSuit() {
+    return suit.printSuit();
+  }
+
+  public String toString() {
+    String cardString = "";
+    cardString += rank.printRankLabel() + suit.printSuit();
+    return cardString;
+  }
+
+}
--- Rank.java
+++ Rank.java
@@ -1,29 +1,35 @@
 public enum Rank {
   // enumerate card values
-  TWO(2), 
-  THREE(3),
-  FOUR(4),
-  FIVE(5),
-  SIX(6),
-  SEVEN(7),
-  EIGHT(8),
-  NINE(9),
-  TEN(10),
-  JACK(11),
-  QUEEN(12),
-  KING(13),
-  ACE(14);
+  TWO(2, "2"), 
+  THREE(3, "3"),
+  FOUR(4, "4"),
+  FIVE(5, "5"),
+  SIX(6, "6"),
+  SEVEN(7, "7"),
+  EIGHT(8, "8"),
+  NINE(9, "9"),
+  TEN(10, "10"),
+  JACK(11, "Jack"),
+  QUEEN(12, "Queen"),
+  KING(13, "King"),
+  ACE(14, "Ace");
 
   // prevent changes to Rank during runtime
   private final int rankValue;
+  private final String rankLabel;
 
   // assign value to members of Rank
-  private Rank(int rankValue) {
+  private Rank(int rankValue, String rankLabel) {
     this.rankValue = rankValue;
+    this.rankLabel = rankLabel;
   }
 
   // expose Rank as public method
-  public int printRank() {
+  public int getRankValue() {
     return rankValue;
+  }
+
+  public int printRankLabel() {
+    return rankLabel;
   }
 }
