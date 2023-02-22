5db5ca8f654bdfa4f8a8f27e67e0480a2d140967
Cynthia Black <cindycblack@gmail.com>
2023-02-21 17:06:32 -0900

    add Deck class, change Ace value to 1

---
+++ Deck.java
@@ -0,0 +1,10 @@
+public class Deck extends Hand {
+  public void populate() {
+    for (Suit suit: Suit.values()) {
+      for (Rank rank: Rank.values()) {
+        Card card = new Card(rank, suit);
+        this.add(card);
+      }
+    }
+  }
+}
--- Rank.java
+++ Rank.java
@@ -1,5 +1,6 @@
 public enum Rank {
   // enumerate card values
+  ACE(1, "Ace"),
   TWO(2, "2"),
   THREE(3, "3"),
   FOUR(4, "4"),
@@ -11,8 +12,7 @@ public enum Rank {
   TEN(10, "10"),
   JACK(11, "Jack"),
   QUEEN(12, "Queen"),
-  KING(13, "King"),
-  ACE(14, "Ace");
+  KING(13, "King");
 
   // prevent changes to Rank during runtime
   private final int rankValue;
