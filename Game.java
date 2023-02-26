4b0ddfc3a1334c2a3934911b0ea212d28ae44cc4
Cynthia Black <cindycblack@gmail.com>
2023-02-26 12:28:55 -0900

    Change user discard to user hold

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
@@ -24,65 +25,60 @@ public class Game {
   while (playerHand.getCards().size() < 5) {
     gameDeck.dealCard(playerHand);
   }
-  System.out.println("Your hand is: ");
+  System.out.println("\n" + "Your hand is: ");
   for (int i = 0; i < playerHand.getCards().size(); i++) {
       System.out.println((i + 1) + ": " + playerHand.getCards().get(i).toString());
     }
   
   Scanner input = new Scanner(System.in);
 
-  // discard a card
-  System.out.println("Enter the numbers of the cards you want to discard (separated by spaces), enter -1 to discard all cards, or enter 0 to hold all cards in your current hand:");
+  // player chooses which cards to hold
+  System.out.println("\n" + "Enter the numbers of the cards you want to hold (separated by spaces) or enter 0 to hold all cards in your current hand:");
   String indicesString = input.nextLine();
-  String[] indicesArray = indicesString.split("\\s+");
-  int[] indices = new int[indicesArray.length];
+  String[] indicesArray = indicesString.split("\s+");
+  ArrayList<Integer> indicesToKeep = new ArrayList<Integer>();
 
   for (int i = 0; i < indicesArray.length; i++) {
-    indices[i] = Integer.parseInt(indicesArray[i]);
-  }
-
-  Arrays.sort(indices);
-  Hand discardHand = new Hand();
-  EvaluateHand evaluator = new EvaluateHand();
-
-  for (int i = indices.length - 1; i >= 0; i--) {
-    int index = indices[i];
-    if (index == 0) {
-      System.out.println("Evaluating hand.");
-      String handResult = evaluator.evaluateHand(playerHand.getCards().toArray(new Card[playerHand.getCards().size()]));
-      System.out.println("Your hand is a " + handResult + ".");
+    int index = Integer.parseInt(indicesArray[i]);
+    
+    while (index < 1 || index > playerHand.getCards().size()) {
+      System.out.println("Invalid input. Please try again.");
     }
-    else if (index < 1 || index > playerHand.getCards().size()) {
-      System.out.println("Invalid index.");
+    if (index == 0) {
+      // System.out.println("Evaluating hand.");
+      break;
     }
     else {
-      Card card = playerHand.getCards().get(index - 1);
-      boolean success = playerHand.discard(card, discardHand);
-
-      if (success) {
-        System.out.println("You discarded " + card);
-      }
-      else {
-        System.out.println("Could not discard selected card!");
-      }
+      indicesToKeep.add(index);
     }
   }
-
-  // replace discards in playerHand
-  while (playerHand.getCards().size() < 5) {
-    gameDeck.dealCard(playerHand);
+    
+  // create new hand with held cards
+  Hand heldHand = new Hand();
+  for (int i = 0; i < indicesToKeep.size(); i++) {
+    int index = indicesToKeep.get(i);
+    Card card = playerHand.getCards().get(index - 1);
+    heldHand.add(card);
   }
 
+  // ensure heldHand is a full hand
+  while (heldHand.getCards().size() < 5) {
+    gameDeck.dealCard(heldHand);
+  }
+
+  // evaluate heldHand
+  EvaluateHand evaluator = new EvaluateHand();
+
   System.out.println("Your hand is: ");
-  System.out.println(playerHand.showHand());
+  System.out.println(heldHand.showHand());
   System.out.println("Evaluating hand.");
-      String handResult = evaluator.evaluateHand(playerHand.getCards().toArray(new Card[playerHand.getCards().size()]));
-      System.out.println("Your hand is a " + handResult + ".");
+      String handResult = evaluator.evaluateHand(heldHand.getCards().toArray(new Card[heldHand.getCards().size()]));
+      System.out.println("Your hand is " + handResult + ".");
 }
 
   public void bet(int amount, Scanner input) {
       playerBank.subtractCredits(amount);
-      System.out.println("Betting " + amount + " credit(s).");
+      System.out.println("\n" + "Betting " + amount + " credit(s).");
     }
 
   public static void main(String[] args) {
@@ -92,7 +88,7 @@ public class Game {
     
     int betAmount = 0;
     do {
-      System.out.print("Enter a bet of 1 or more credits to start the game or type 0 to exit.");
+      System.out.print("\n" + "Enter a bet of 1 or more credits to start the game or type 0 to exit.");
       betAmount = scanner.nextInt();
       if (betAmount > 0) {
         game.bet(betAmount, scanner);
