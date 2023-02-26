4040ad4b7573f83c26c0c91be9918b6d85abe16e
Cynthia Black <cindycblack@gmail.com>
2023-02-25 20:26:12 -0900

    Add remaining hands to EvaluateHand

--- EvaluateHand.java
+++ EvaluateHand.java
@@ -6,10 +6,10 @@ public class EvaluateHand {
     // sort the cards by both rank and suit
     Arrays.sort(hand, Comparator.comparing((Card c) -> c.getSuitName()).thenComparingInt((Card c) -> c.getRank().getRankValue()));
 
-    // check for Ace
+    // check for ace & king
     boolean hasAce = hand[0].getRank().equals(Rank.ACE);
     boolean hasKing = hand[4].getRank().equals(Rank.KING);
-
+    
     // treat Ace as high if hand has both Ace & King
     if (hasAce && hasKing) {
       hand[0].setRankValue(14);
@@ -58,6 +58,53 @@ public class EvaluateHand {
     if (straight) {
       return "Straight";
     }
-  return "You had another hand.";
+
+    // check for 4 of a kind
+    if (hand[0].getRank() == hand[3].getRank() || hand[1].getRank() == hand[4].getRank()) {
+      return "Four of a Kind";
+    }
+
+    // check for full house
+    if ((hand[0].getRank() == hand[1].getRank() && hand[2].getRank() == hand[4].getRank()) || (hand[0].getRank() == hand[2].getRank() && hand[3].getRank() == hand[4].getRank())) {
+      return "Full House";
+    }
+
+    // check for 3 of a kind
+    if (hand[0].getRank() == hand[2].getRank() || hand[1].getRank() == hand[3].getRank() || hand[2].getRank() == hand[4].getRank()) {
+      return "Three of a Kind";
+    }
+
+    // check for two pair
+    int pairCount = 0;
+    for (int i = 0; i < hand.length - 1; i++) {
+      if (hand[i].getRank() == hand[i+1].getRank()) {
+        pairCount++;
+        i++;
+      }
+    }
+    if (pairCount == 2) {
+      return "Two Pair";
+    }
+
+    pairCount = 1;
+    Rank currentRank = hand[0].getRank();
+    for (int i = 1; i < hand.length; i++) {
+      if (hand[i].getRank() == currentRank) {
+        pairCount++;
+      } else {
+        if (pairCount >= 2 && (currentRank == Rank.JACK || currentRank == Rank.QUEEN || currentRank == Rank.KING || currentRank == Rank.ACE)) {
+          return "Jacks or Better";
+        }
+        currentRank = hand[i].getRank();
+        pairCount = 1;
+      }
+    }
+    if (pairCount >= 2 && (currentRank == Rank.JACK || currentRank == Rank.QUEEN || currentRank == Rank.KING || currentRank == Rank.ACE)) {
+      return "Jacks or Better";
+    }
+
+
+    
+  return "not a winner";
   }
 }
