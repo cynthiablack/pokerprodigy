c64c0684ef95ea884f2bc93b2f66679a86e104d0
Cynthia Black <cindycblack@gmail.com>
2023-02-27 18:16:20 -0900

    Refactor hand evaluation to correctly detect 2 Pair, straights

--- EvaluateHand.java
+++ EvaluateHand.java
@@ -22,8 +22,8 @@ public class EvaluateHand {
     int kingCount = 0;
 
     // count the instances of each rank
-    Rank currentRank = hand[0].getRank();
-    for (int i = 1; i < hand.length; i++) {
+    //Rank currentRank = hand[0].getRank();
+    for (int i = 0; i < hand.length; i++) {
       if (hand[i].getRank() == Rank.ACE) {
         aceCount++;
       } 
@@ -96,70 +96,26 @@ public class EvaluateHand {
         spadesCount == 5) {
       sameSuit = true;
     }
-    
-    boolean hasAce = hand[0].getRank().equals(Rank.ACE);
-    boolean hasKing = hand[4].getRank().equals(Rank.KING);
-    
-    // treat Ace as high if hand has both Ace & King
-    if (hasAce && hasKing) {
-      hand[0].setRankValue(14);
-    }
-
-    // check for royal flush
-    if (sameSuit &&
-        hand[0].getRank() == Rank.ACE &&
-        hand[1].getRank() == Rank.TEN &&
-        hand[2].getRank() == Rank.JACK &&
-        hand[3].getRank() == Rank.QUEEN &&
-        hand[4].getRank() == Rank.KING) {
-          return "Royal Flush";
-        }
 
     // check for straight
-    boolean straight = true;
-    for (int i = 0; i < hand.length - 1; i++) {
-      if (hand[i].getRank().getRankValue() != hand[i+1].getRank().getRankValue() -1) {
-        straight = false;
-        break;
-      }
-    }
+boolean straight = false;
+Arrays.sort(hand, Comparator.comparing(Card::getRank));
+for (int i = 0; i < hand.length - 4; i++) {
+  if (hand[i].getRank().getRankValue() == hand[i+1].getRank().getRankValue() - 1 &&
+      hand[i+1].getRank().getRankValue() == hand[i+2].getRank().getRankValue() - 1 &&
+      hand[i+2].getRank().getRankValue() == hand[i+3].getRank().getRankValue() - 1 &&
+      hand[i+3].getRank().getRankValue() == hand[i+4].getRank().getRankValue() - 1) {
+    straight = true;
+    break;
+  }
+}
 
-    // check for straight flush
-    if (sameSuit && straight) {
-      return "Straight Flush"; 
-    }
 
-    // check for flush
-    if (sameSuit) {
-      return "Flush";
-    }
-
-    // payout straight
-    if (straight) {
-      return "Straight";
-    }
-      
-    // check for four of a kind
-    if (aceCount == 4 || 
-        twoCount == 4 || 
-        threeCount == 4 || 
-        fourCount == 4 || 
-        fiveCount == 4 || 
-        sixCount == 4 || 
-        sevenCount == 4 || 
-        eightCount == 4 || 
-        nineCount == 4 || 
-        tenCount == 4 || 
-        jackCount == 4 || 
-        queenCount == 4 || 
-        kingCount == 4) {
-          return "Four of a Kind";
-        }
-
+    // boolean checks: three of a kind, pair
     boolean threeOfAKind = false;
     boolean pair = false;
-    
-    if (aceCount == 3 || 
+
+      if (aceCount == 3 || 
         twoCount == 3 || 
         threeCount == 3 || 
         fourCount == 3 || 
@@ -175,7 +131,7 @@ public class EvaluateHand {
           threeOfAKind = true;
         }
 
-        if (aceCount == 2 || 
+      if (aceCount == 2 || 
         twoCount == 2 || 
         threeCount == 2 || 
         fourCount == 2 || 
@@ -191,32 +147,92 @@ public class EvaluateHand {
           pair = true;
         }
 
-    // check for full house
-    if (threeOfAKind && pair) {
-      return "Full House";
-    }
-    // check for three of a kind
-    if (threeOfAKind) {
-      return "Three of a Kind";
-    }
-
-    // check for two pair
+    // count number of pairs
     int pairCount = 0;
-    for (int i = 0; i < hand.length - 1; i++) {
-      if (hand[i].getRank() == hand[i+1].getRank()) {
-        pairCount++;
-        i++;
+    for (int i = 0; i < hand.length; i++) {
+      for (int j = i + 1; j < hand.length; j++) {
+        if (hand[i].getRank() == hand[j].getRank()) {
+          pairCount++;
+        }
       }
     }
-    if (pairCount == 2) {
-      return "Two Pair";
-    }
 
-  // check for jacks or better
-  if (jackCount > 1 || queenCount > 1 || kingCount > 1 || kingCount > 1) {
-    return "Jacks or Better";
-  }
+    // check hand for win/lose
+    if (sameSuit) {
+      boolean hasAce = hand[0].getRank().equals(Rank.ACE);
+      boolean hasKing = hand[4].getRank().equals(Rank.KING);
     
-  return "not a winner";
+      // treat Ace as high if hand has both Ace & King
+      if (hasAce && hasKing) {
+        hand[0].setRankValue(14);
+      }
+
+      // check for royal flush
+      if (hand[0].getRank() == Rank.ACE &&
+          hand[1].getRank() == Rank.TEN &&
+          hand[2].getRank() == Rank.JACK &&
+          hand[3].getRank() == Rank.QUEEN &&
+          hand[4].getRank() == Rank.KING) {
+            return "Royal Flush";
+          }
+      
+      // check for straight flush
+      else if (straight) {
+        return "Straight Flush"; 
+      }
+
+      // check for flush
+      else {
+        return "Flush";
+      }
+    }
+    else {
+      // payout straight
+      if (straight) {
+        return "Straight";
+      }
+
+      // check for four of a kind
+      else if (aceCount == 4 || 
+          twoCount == 4 || 
+          threeCount == 4 || 
+          fourCount == 4 || 
+          fiveCount == 4 || 
+          sixCount == 4 || 
+          sevenCount == 4 || 
+          eightCount == 4 || 
+          nineCount == 4 || 
+          tenCount == 4 || 
+          jackCount == 4 || 
+          queenCount == 4 || 
+          kingCount == 4) {
+            return "Four of a Kind";
+          }
+
+      // check for full house
+      else if (threeOfAKind && pair) {
+        return "Full House";
+      }
+      // check for three of a kind
+      else if (threeOfAKind) {
+        return "Three of a Kind";
+      }
+  
+      // check for two pair
+      
+      else if (pairCount == 2) {
+        return "Two Pair";
+      }
+        
+      // check for jacks or better
+      else if (jackCount > 1 || queenCount > 1 || kingCount > 1 || aceCount > 1) {
+        return "Jacks or Better";
+      }
+
+      // if none of the above conditons are met, not a winnner
+      else {
+        return "not a winner";
+      }
   }
+    }
 }
